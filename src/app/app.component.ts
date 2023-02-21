4c4ef0b86d40f84516fcae828ccb6ef0c8abfe62
Jaikumar248 <jaikumaryh248@>
2023-02-21 21:34:20 +0530

    practicing on Angular DataBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-application';
-}
+  
+//   isdisabled:boolean = false;
+
+//  ishidden:boolean = false;
+// isactive:boolean = true;
+// cvar:string = 'blue';
+// mystyle:object = {
+//   color:'green',
+//   background:'grey',
+//   border: '4px solid black'
+// }
+// name:'xyz'
+// counter: number = 0;
+// increment() {
+//   this.counter+=1;
+// }
+// decrement() {
+//   this.counter-=1;
+// }
+// changeName(e:any) {
+//   this.name = e.target.value;
+// }
+// city = "bangalore";
+// updateCity() {
+//   this.city='hebbal'
+// }
+// show:boolean = false;
+// movies = ['RRR','BhemlaNayak','puspha']
+// data=["abc","bca","ssdd"]
+//  nums=[1,2,3,4,5,6,7,8,9,10];
+// nums=[
+//   {
+//     "id":1,
+//     "name":"oneplus",
+//     "price": 23300,
+//     "ram": "124gb"
+//   },
+//   {
+//     "id":3,
+//     "name":"oneplus1",
+//     "price": 50000,
+//     "ram": "128gb"
+//   }
+// ]
+num1: number = 4;
+num2: number = 6;
+op: string=''
+
+
+ }
